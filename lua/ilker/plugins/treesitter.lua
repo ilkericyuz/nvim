11f22bc0c8419970e1ce7071101f2bdc00614bf7
ilker <ilker@demets-macbook-pro.tailcd6f7d.ts.net>
2024-09-15 15:36:14 +0200

    feat: configure treesitter incremental selection

--- lua/ilker/plugins/treesitter.lua
+++ lua/ilker/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
+  event = { 'BufReadPre', 'BufNewFile' },
   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
@@ -39,11 +40,20 @@ return { -- Highlight, edit, and navigate code
       additional_vim_regex_highlighting = { 'ruby' },
     },
     indent = { enable = true, disable = { 'ruby' } },
+    --    - incremental selection: included, see `:help nvim-treesitter-incremental-selection-mod`
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = 'gnn',
+        node_incremental = 'gnn',
+        scope_incremental = 'false',
+        node_decremental = '<bs>',
+      },
+    },
   },
   -- There are additional nvim-treesitter modules that you can use to interact
   -- with nvim-treesitter. You should go explore a few and see what interests you:
   --
-  --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
   --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
   --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 }
